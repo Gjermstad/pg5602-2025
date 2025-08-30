74d3deafb600dbf20c1f5bf84c35d385615c5f18
Kenneth Andre Bettum Gjermstad <keanbegj@gmail.com>
2025-08-30 23:23:47 +0200

    HW01: Start

---
+++ assignments/01-homework-state-dices/01-homework-state-dices/HomeworkStateDicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_homework_state_dicesApp.swift
+//  01-homework-state-dices
+//
+//  Created by Kenneth Andre Bettum Gjermstad on 30/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct HomeworkStateDicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ assignments/01-homework-state-dices/01-homework-state-dices/MainView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  01-homework-state-dices
+//
+//  Created by Kenneth Andre Bettum Gjermstad on 30/08/2025.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
