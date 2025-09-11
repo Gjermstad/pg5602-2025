7104f1d96a3fa0be275ce892fcfbf1c18eba9488
Kenneth Andre Bettum Gjermstad <keanbegj@gmail.com>
2025-09-11 11:16:01 +0200

    Initial commit

--- assignments/03-homework-ratatouille/03-homework-ratatouille/Views/Recipes/RecipesDetails.swift
+++ assignments/03-homework-ratatouille/03-homework-ratatouille/Views/Recipes/RecipesDetails.swift
@@ -7,9 +7,11 @@
 
 import SwiftUI
 
-struct RecipesDetails: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct RecipesDetails: View
+{
+    var body: some View
+  {
+        Text("RecipesDetails")
     }
 }
 
