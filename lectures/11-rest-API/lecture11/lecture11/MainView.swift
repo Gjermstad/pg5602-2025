7ee46bc13fe6c0962658efcdaf19b4b2b4e6bf3f
Kenneth A. Bettum Gjermstad <79542593+Gjermstad@users.noreply.github.com>
2025-10-08 09:42:34 +0200

    l11 - Lecture start

---
+++ lectures/11-rest-API/lecture11/lecture11/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  lecture11
+//
+//  Created by Kenneth Andre Bettum Gjermstad on 08/10/2025.
+//
+
+import SwiftUI
+
+struct MainView: View
+{
+  var body: some View
+  {
+    
+  }
+}
+
+#Preview {
+  MainView()
+}
---
+++ lectures/11-rest-API/lecture11/lecture11/lecture11App.swift
@@ -0,0 +1,20 @@
+//
+//  lecture11App.swift
+//  lecture11
+//
+//  Created by Kenneth Andre Bettum Gjermstad on 08/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct lecture11App: App
+{
+  var body: some Scene
+  {
+    WindowGroup
+    {
+      MainView()
+    }
+  }
+}
