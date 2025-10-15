1a593918c66fa8eb9164483032cab79ebe07a713
Kenneth A. Bettum Gjermstad <79542593+Gjermstad@users.noreply.github.com>
2025-10-15 10:57:51 +0200

    l13: New code for FruityVice API

---
+++ lectures/13-fruityvice/FruityVice/FruityVice/FruityViceApp.swift
@@ -0,0 +1,20 @@
+//
+//  FruityViceApp.swift
+//  FruityVice
+//
+//  Created by Kenneth Andre Bettum Gjermstad on 15/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct FruityViceApp: App
+{
+  var body: some Scene
+  {
+    WindowGroup
+    {
+      MainView()
+    }
+  }
+}
---
+++ lectures/13-fruityvice/FruityVice/FruityVice/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  FruityVice
+//
+//  Created by Kenneth Andre Bettum Gjermstad on 15/10/2025.
+//
+
+import SwiftUI
+
+struct MainView: View
+{
+  var body: some View
+  {
+    
+  }
+}
+
+#Preview {
+  MainView()
+}
