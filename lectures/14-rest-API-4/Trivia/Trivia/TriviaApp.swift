0c50c0fa7da0914b94222aef1cc0cbcb105ee37c
Kenneth A. Bettum Gjermstad <79542593+Gjermstad@users.noreply.github.com>
2025-10-16 09:27:46 +0200

    l14: Initial commit, starting from scratch this lecture

---
+++ lectures/14-rest-API-4/Trivia/Trivia/TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaApp.swift
+//  Trivia
+//
+//  Created by Kenneth Andre Bettum Gjermstad on 16/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
